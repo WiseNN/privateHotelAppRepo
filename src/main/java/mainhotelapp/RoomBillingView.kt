97e24f3dd86430beb98a604b7428b4e37d5536fe
Norris Wise Jr <wisen@WiseNN-MacBook-Pro.local>
2018-03-21 01:19:42 -0400

    added billing html template

--- src/main/java/mainhotelapp/RoomBillingView.kt
+++ src/main/java/mainhotelapp/RoomBillingView.kt
@@ -3,19 +3,32 @@ package mainhotelapp
 import couchdb.Room
 import javafx.scene.Parent
 import javafx.scene.layout.AnchorPane
+import javafx.scene.web.WebView
 import tornadofx.*
 
 class RoomBillingView(room : Room) : Fragment()
 {
     private val rootFXML: AnchorPane by fxml("/fxml/RoomBillingUI.fxml")
-
+    private val billingWebView : WebView by fxid()
 
     override val root =  rootFXML
 
     val room = room
 
+
+
     init{
 
         println("RoomBilling Room Number: ${room.roomNumber}")
+//        room.roomNumber
+//        room.notes
+//        room.stayDuration
+//        room.hasPetProperty
+//        room.isSmoking
+//        room.bedType
+//        room.additionalPackages
+
+
+        billingWebView.engine.executeScript("")
     }
 }
